69c1d77bf59c03a2cfefe3757c429f5e0a73e547
kr-jaysavani <jay.s@knackroot.com>
2024-01-31 10:04:50 +0530

    routing

---
+++ next-app/app/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+     <h1>Venom</h1> 
+     <Link href={'/user'}>Go to userPage</Link>
+    </main>
+  )
+}
---
+++ next-app/app/user/page.tsx
@@ -0,0 +1,9 @@
+
+export default function User() {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+       <h1>User</h1> 
+      </main>
+    )
+  }
+  
